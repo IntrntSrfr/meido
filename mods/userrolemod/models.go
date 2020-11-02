72cd64dc24354db9097161e988a4d316f56dc8e9
IntrntSrfr <aleksander@tosbakken.com>
2020-11-02 01:22:00 +0100

    test

--- mods/userrolemod/models.go
+++ mods/userrolemod/models.go
@@ -2,7 +2,7 @@ package userrolemod
 
 type Userrole struct {
 	UID     int
-	GuildID int64
-	RoleID  int64
-	UserID  int64
+	GuildID int64 `db:"guild_id"`
+	RoleID  int64 `db:"role_id"`
+	UserID  int64 `db:"user_id"`
 }
