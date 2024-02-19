6901d883e68cba00570a100b12c50bf2876fa0ce
IntrntSrfr <aleksander@tosbakken.com>
2024-02-19 16:27:06 +0100

    convenience response methods

--- pkg/mio/discord/discord_interaction.go
+++ pkg/mio/discord/discord_interaction.go
@@ -1,6 +1,7 @@
 package discord
 
 import (
+	"io"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -23,6 +24,31 @@ func (it *DiscordInteraction) Respond(text string) error {
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Content: text,
+		},
+	}
+	return it.Sess.InteractionRespond(it.Interaction, resp)
+}
+
+func (it *DiscordInteraction) RespondEphemeral(text string) error {
+	resp := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: text,
+			Flags:   discordgo.MessageFlagsEphemeral,
+		},
+	}
+	return it.Sess.InteractionRespond(it.Interaction, resp)
+}
+
+func (it *DiscordInteraction) RespondFile(text, name string, reader io.Reader) error {
+	resp := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: text,
+			Files: []*discordgo.File{{
+				Name:   name,
+				Reader: reader,
+			}},
 		},
 	}
 	return it.Sess.InteractionRespond(it.Interaction, resp)
